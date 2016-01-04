38f98e5afa46ae24576a99aa77a92c69ed5746d0
raywash <ray@sharay.com>
2016-01-04 15:14:30 -0500

    Add CRUD pages to app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
   end
 end
+private
+  # Rails Strong Params
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :first_name,
+      :last_name,
+      :password,
+      :password_confirmation
+    )
+  end
+end    
