0e3056e5df9d586fec99af103308dc7fcfa556f1
raywash <ray@sharay.com>
2016-01-07 11:06:48 -0500

    Fix missing template issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: "You've been registered sucessfully!"
+    else
+      render :new  
   end
 end
 private
@@ -18,4 +27,4 @@ private
       :password_confirmation
     )
   end
-end    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'readings#index'
+  root 'users#index'
   resources :users
 
   # Example of regular route:
