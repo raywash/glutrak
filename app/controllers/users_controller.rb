483a65256ad09849c35da0ff1f128066e0aebdec
raywash <ray@sharay.com>
2016-01-08 13:20:33 -0500

    Edit and Back links working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,30 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path, notice: "You've been registered sucessfully!"
+      redirect_to users_path, notice: "You've been registered sucessfully!"
     else
-      render :new  
+      render :new
   end
 end
+
+def edit
+  @user = User.find(params[:id])
+end
+
+def show
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+
+  if @user.update_attributes(user_params)
+    redirect_to users_path, notice: "You've been edited sucessfully!"
+  else
+    render :edit
+  end  
+end
+
 private
   # Rails Strong Params
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'readings#index'
   resources :users
 
   # Example of regular route:
